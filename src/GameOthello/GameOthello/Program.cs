6087bb2e4854084d63f2f42a744e79dbb5c95c18
Marco Esquivel <marcoev3011@hotmail.com>
2018-08-13 15:16:30 -0600

    backend
    backend v1

---
+++ src/GameOthello/GameOthello/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GameOthello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            juego Game = new juego();
+            Game.crearTablero(8);
+            Game.imprimir(8);
+
+            while (true)
+            {
+                Console.WriteLine("********************************JUGADOR CON LA FICHA 1*****************************");
+                Console.WriteLine("Digite un fila");
+                int fila = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite una columna");
+
+                int columna = Convert.ToInt32(Console.ReadLine());
+
+                Game.movimiento(fila, columna, 1);
+
+                Game.imprimir(8);
+
+                Console.WriteLine("********************************JUGADOR CON LA FICHA 2*****************************");
+
+                Console.WriteLine("Digite un fila");
+                int fila2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite una columna");
+
+                int columna2 = Convert.ToInt32(Console.ReadLine());
+
+                Game.movimiento(fila2, columna2, 2);
+
+                Game.imprimir(8);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
